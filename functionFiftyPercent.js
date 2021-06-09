25e6b505fea440ccc0d947b82033dfe8753c082f
Gavry <lady.sonia@bk.ru>
2021-06-09 13:04:46 +0300

    upload filees

---
+++ functionFiftyPercent.js
@@ -0,0 +1,20 @@
+function fiftyPercent(obj) {
+    let result
+    let price
+    for (k in obj){ 
+    price = obj[k];
+    result = price + price/100*15;
+    result = Math.ceil(result/5)*5;
+    obj[k] = result;
+    console.log(obj[k]);
+    }
+  }
+
+let product = {
+    "apple": 800,
+    "banana": 600,
+    "milk": 700
+  };
+
+fiftyPercent(product); 
+console.log(product);
